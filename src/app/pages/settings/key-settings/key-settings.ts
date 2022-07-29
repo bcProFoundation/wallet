f35a8d49cc2c300c8724bec582d2cd81cd6e2053
Binh Vo <kenneth.vo8x@gmail.com>
2022-07-29 11:50:46 +0700

    Add translate for method unlock app screen

--- src/app/pages/settings/key-settings/key-settings.ts
+++ src/app/pages/settings/key-settings/key-settings.ts
@@ -345,8 +345,7 @@ export class KeySettingsPage {
         try {
           keys = this.keyProvider.get(this.keyId, password);
         } catch (err) {
-          const title =
-            'Your wallet is in a corrupt state. Please contact support and share the logs provided.';
+          const title = this.translate.instant('Your account is in a corrupt state. Please contact support and share the logs provided.');
           let message;
           try {
             message =

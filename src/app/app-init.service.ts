5328fa0e6c1a2e29d883b8e8c2aff64a4ba57e9f
takYoon <tan8651913@gmail.com>
2022-08-26 10:38:56 +0700

    update code for app-init

---
+++ src/app/app-init.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AppInitService {
+
+  constructor(private Router: Router) {}
+
+  init() {
+    return new Promise<void>((resolve, reject) => {
+
+        // Simple example from an array. In reality, I used the response of
+        // a GET. Important thing is that the app will wait for this promise to resolve
+        // const newDynamicRoutes = [{
+        //     routeName: '',
+        //     component: 'SwapPage'
+        // }]
+        const routes = this.Router.config;
+        routes.push({ path: '', component: SwapPage });
+        this.Router.resetConfig(routes);
+        resolve();
+    });
+  }
+}

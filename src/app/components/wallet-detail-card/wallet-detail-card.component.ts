7238427a75769417123091561d3aef0f20d70e75
ericson <phamnguyentanh.97@gmail.com>
2023-02-21 11:40:09 +0700

    fix ui footer page confirm & add icon MVP token

--- src/app/components/wallet-detail-card/wallet-detail-card.component.ts
+++ src/app/components/wallet-detail-card/wallet-detail-card.component.ts
@@ -72,7 +72,7 @@ export class WalletDetailCardComponent {
 
   public currentTheme: string;
   public hiddenBalance: boolean;
-  listEToken = ['EAT', 'DoC', 'bcPro', 'LPSe', 'eHNL', 'eLPS', 'USDR'];
+  listEToken = ['EAT', 'DoC', 'bcPro', 'LPSe', 'eHNL', 'eLPS', 'USDR', "🎖MVP"];
   public typeErrorQr = NgxQrcodeErrorCorrectionLevels;
   public loading: boolean;
   public newAddressError: boolean;
--- src/app/pages/wallets/wallets.ts
+++ src/app/pages/wallets/wallets.ts
@@ -59,7 +59,7 @@ export class WalletsPage {
   public symbolCurrency;
   public keyHiddenBalanceTemp = [];
   public flagOptionRemove: boolean;
-  listEToken = ['EAT', 'DoC', 'bcPro', 'LPSe', 'eHNL', 'eLPS', 'USDR'];
+  listEToken = ['EAT', 'DoC', 'bcPro', 'LPSe', 'eHNL', 'eLPS', 'USDR', "🎖MVP"];
   donationSupportCoins = [];
   navParamsData;
   isShowCreateNewWallet = false;
